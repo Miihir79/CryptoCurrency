46eddaa5c8252e32c4c65f6fcb15d26ba55397dc
Miihir79 <mihirrshah02@gmail.com>
2021-09-11 17:57:19 +0530

    UI complete

--- app/src/main/java/com/mihir/cryptocurrency/domain/use_cases/get_coin/GetCoinsUseCase.kt
+++ app/src/main/java/com/mihir/cryptocurrency/domain/use_cases/get_coin/GetCoinsUseCase.kt
@@ -13,13 +13,13 @@ import javax.inject.Inject
 class GetCoinsUseCase @Inject constructor(private val repository:CoinRepository) {
     operator fun invoke(coinId:String): Flow<Resource<CoinDetail>> = flow {
         try {
-            emit(Resource.Loading())
+            emit(Resource.Loading<CoinDetail>())
             val coin  = repository.getCoinById(coinId).toCoinDetail()
-            emit(Resource.Success(coin))
+            emit(Resource.Success<CoinDetail>(coin))
         }catch (e:HttpException){
-            emit(Resource.Error(e.localizedMessage?:"An unexpected error occurred"))
+            emit(Resource.Error<CoinDetail>(e.localizedMessage?:"An unexpected error occurred"))
         }catch (e: IOException){
-            emit(Resource.Error("Couldn't reach the server"))
+            emit(Resource.Error<CoinDetail>("Couldn't reach the server"))
         }
     }
 }
--- app/src/main/java/com/mihir/cryptocurrency/domain/use_cases/get_list/GetListUserCase.kt
+++ app/src/main/java/com/mihir/cryptocurrency/domain/use_cases/get_list/GetListUserCase.kt
@@ -13,13 +13,13 @@ import javax.inject.Inject
 class GetListUserCase @Inject constructor(private val repository: CoinRepository) {
     operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
         try {
-            emit(Resource.Loading())
+            emit(Resource.Loading<List<Coin>>())
             val coins  = repository.getCoins().map { it.toCoin() }
-            emit(Resource.Success(coins))
+            emit(Resource.Success<List<Coin>>(coins))
         }catch (e: HttpException){
-            emit(Resource.Error(e.localizedMessage?:"An unexpected error occurred"))
+            emit(Resource.Error<List<Coin>>(e.localizedMessage?:"An unexpected error occurred"))
         }catch (e: IOException){
-            emit(Resource.Error("Couldn't reach the server"))
+            emit(Resource.Error<List<Coin>>("Couldn't reach the server"))
         }
     }
 }
